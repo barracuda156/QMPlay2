5895371bdd24f60f760123a6e67d02946c6b7041
Sergey Fedorov <vital.had@gmail.com>
2025-05-24 05:41:46 +0800

    YouTube.cpp: backport fbb854d

--- src/modules/Extensions/YouTube.cpp
+++ src/modules/Extensions/YouTube.cpp
@@ -1101,6 +1101,8 @@ QStringList YouTube::getYouTubeVideo(const QString &param, const QString &url, I
         auto subtitlesForLang = subtitles[lang].toArray();
         if (subtitlesForLang.isEmpty())
             subtitlesForLang = subtitles[QMPlay2Core.getLanguage()].toArray();
+        if (subtitlesForLang.isEmpty())
+            subtitlesForLang = subtitles["en"].toArray();
 
         for (auto &&subtitlesFmtVal : asConst(subtitlesForLang))
         {
